c0ce0b98997bc2707213a4b3752d05fc18c1c7ed
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-02-01 10:51:11 -0700

    Widget
    Creación de un textview y enlazarlo al código

--- EVA1_2_WIDGETS/app/src/main/java/com/example/miguel/eva1_2_widgets/Principal.java
+++ EVA1_2_WIDGETS/app/src/main/java/com/example/miguel/eva1_2_widgets/Principal.java
@@ -2,12 +2,18 @@ package com.example.miguel.eva1_2_widgets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
+
+    private TextView txtVwMensa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        txtVwMensa = findViewById(R.id.txtVwMensa);
+        txtVwMensa.setText(R.string.mi_recurso);
     }
 }
